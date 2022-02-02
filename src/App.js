47a8a5cb5977f7d40107adf71f31f9ffa9c96c7d
cliffmax85 <cliffmax85@gmail.com>
2022-02-01 16:38:03 -0800

    added drink dropdown js and updated app js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import { useState } from 'react';
 import './App.css';
 import Food from './Food';
 import FoodDropwdown from './FoodDropwdown';
+import DrinkDropdown from './DrinkDropdown';
 
 function App() {
   const [foodId, setFoodId] = useState(1);
+  const [drinkId, setDrinkId] = useState(1);
 
   return (
     <div className="App">
-      <Food
-        foodId={foodId} />
+      <Food 
+        foodId={foodId}
+        drinkId={drinkId} />
+
       <h1>
         Please Choose Your Meal add name here
       </h1>
       <div>
         <section className='dropdowns'>
           <FoodDropwdown setFoodId={setFoodId} />
+          <DrinkDropdown setDrinkId={setDrinkId} />
         </section>
       </div>
     </div>
