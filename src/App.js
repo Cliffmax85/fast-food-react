d5182ee5766e78725734e418608a684b9bcecf68
cliffmax85 <cliffmax85@gmail.com>
2022-02-01 16:19:58 -0800

    added main dish dropdown and image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import { useState } from 'react';
 import './App.css';
+import Food from './Food';
+import FoodDropwdown from './FoodDropwdown';
 
 function App() {
+  const [foodId, setFoodId] = useState(1);
+
   return (
     <div className="App">
-
+      <Food
+        foodId={foodId} />
+      <h1>
+        Please Choose Your Meal add name here
+      </h1>
+      <div>
+        <section className='dropdowns'>
+          <FoodDropwdown setFoodId={setFoodId} />
+        </section>
+      </div>
     </div>
   );
 }
--- src/Food.js
+++ src/Food.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Food({ foodID, drinkId, sideId }) {
+export default function Food({ foodId, drinkId, sideId }) {
   return <div className='food'>
-      <img className='food-image' src={`food-${foodId}.jpeg`} />
-      <img className='food-image' src={`food-${drinkId}.jpeg`} />
-      <img className='food-image' src={`food-${sideId}.jpeg`} />
+    <img className='food-image' src={`food-${foodId}.jpeg`} />
+    <img className='food-image' src={`drink-${drinkId}.jpeg`} />
+    <img className='food-image' src={`side-${sideId}.jpeg`} />
   </div>;
 }
